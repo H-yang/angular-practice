bb50f8d181b0992188e85d53591c00c650808e6f
H-yang <yhsuan@outlook.com>
2018-10-28 23:57:55 +0000

    test username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Simple ToDo List App';
+  title = 'ToDo List App';
   todos = [
     {
       label: 'Food shopping'
